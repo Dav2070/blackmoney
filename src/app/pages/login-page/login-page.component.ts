b8586071b03b3a80b1f3fea0da6d7013bdd5417d
David <zurek.dav@gmail.com>
2024-04-08 20:23:42 +0200

    Add basic login layout

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,7 +1,23 @@
 import { Component } from "@angular/core"
+import { MatIconModule } from "@angular/material/icon"
+import { MatButtonModule } from "@angular/material/button"
+import { FormsModule } from "@angular/forms"
+import { MatInputModule } from "@angular/material/input"
+import { MatFormFieldModule } from "@angular/material/form-field"
 
 @Component({
 	templateUrl: "./login-page.component.html",
-	styleUrl: "./login-page.component.scss"
+	styleUrl: "./login-page.component.scss",
+	standalone: true,
+	imports: [
+		MatFormFieldModule,
+		MatInputModule,
+		FormsModule,
+		MatButtonModule,
+		MatIconModule
+	]
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+	username = ""
+	password = ""
+}

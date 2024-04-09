16eb2af5b7cd89ec52b4a4d0bf3083282a9767e5
DESKTOP-V6FF5TQ\matvi <matviy.popovich@gmx.de>
2024-04-09 18:26:01 +0200

    Registrierung hinzugefügt

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from "@angular/material/button"
 import { FormsModule } from "@angular/forms"
 import { MatInputModule } from "@angular/material/input"
 import { MatFormFieldModule } from "@angular/material/form-field"
+import { CommonModule } from '@angular/common';
 
 @Component({
 	templateUrl: "./login-page.component.html",
@@ -15,16 +16,40 @@ import { MatFormFieldModule } from "@angular/material/form-field"
 		MatInputModule,
 		FormsModule,
 		MatButtonModule,
-		MatIconModule
+		MatIconModule,
+		CommonModule
 	]
 })
 export class LoginPageComponent {
 	username = ""
 	password = ""
 
+	showRegistrationForm = false; // Diese Variable hinzufügen
+
+	name = ""; // Diese Variablen für das Registrierungsformular hinzufügen
+	email = "";
+	restaurantName = "";
+	address = "";
+	roomCount = 0;
+
 	constructor(private router: Router) {}
 
 	login() {
 		this.router.navigate(["tables"])
 	}
+
+	register() {
+		// Hier implementieren Sie Ihre Registrierungslogik
+		// Zum Beispiel, Sie könnten eine HTTP-Anfrage an Ihren Server senden, um den Benutzer zu registrieren
+		// Wenn die Registrierung erfolgreich ist, könnten Sie den Benutzer weiterleiten oder eine Erfolgsmeldung anzeigen
+	}
+	cancelRegistration() {
+        this.showRegistrationForm = false;
+        // Optional: Zurücksetzen der Registrierungsformulardaten
+        this.name = "";
+        this.email = "";
+        this.restaurantName = "";
+        this.address = "";
+        this.roomCount = 0;
+    }
 }

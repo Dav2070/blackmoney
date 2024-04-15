666ff4c0d354be3591a10fc81e5ef851f1eb2ea6
Adam Wegner <adam.wegner213@gmail.com>
2024-04-15 21:38:09 +0200

    PW Vergessen, diesmal wirklich hinzugefügt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from "@angular/router"
 
 import { LoginPageComponent } from "./pages/login-page/login-page.component"
 import { TableOverviewPageComponent } from "./pages/table-overview-page/table-overview-page.component"
+import { ForgotPasswordPageComponent } from "./pages/forgot-password-page/forgot-password-page.component"
 
 export const routes: Routes = [
 	{
@@ -11,5 +12,9 @@ export const routes: Routes = [
 	{
 		path: "tables",
 		component: TableOverviewPageComponent
+	},
+	{
+		path: "forgot-password",
+		component: ForgotPasswordPageComponent
 	}
 ]

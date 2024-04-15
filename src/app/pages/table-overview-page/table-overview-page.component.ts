f77c674a12748611d8438b3ba54a52a46e0a9696
gruenert <Maik.Gruenert@adesso-insurance-solutions.de>
2024-04-15 16:28:41 +0200

    Layout für Tischansicht erstellt

--- src/app/pages/table-overview-page/table-overview-page.component.ts
+++ src/app/pages/table-overview-page/table-overview-page.component.ts
@@ -1,8 +1,48 @@
-import { Component } from "@angular/core"
+import { Component, OnInit } from "@angular/core"
+import { MatCardModule } from "@angular/material/card"
+import { CommonModule } from "@angular/common"
+import { FormsModule } from "@angular/forms"
+import { MatInputModule } from "@angular/material/input"
+import { MatSelectModule } from "@angular/material/select"
+import { MatFormFieldModule } from "@angular/material/form-field"
+import { MatToolbarModule } from "@angular/material/toolbar"
+
+interface Room {
+	value: string
+	viewValue: string
+	tables: string[]
+}
 
 @Component({
 	templateUrl: "./table-overview-page.component.html",
 	styleUrl: "./table-overview-page.component.scss",
-	standalone: true
+	standalone: true,
+	imports: [
+		MatCardModule,
+		CommonModule,
+		FormsModule,
+		MatInputModule,
+		MatSelectModule,
+		MatFormFieldModule,
+		MatToolbarModule
+	]
 })
-export class TableOverviewPageComponent {}
+export class TableOverviewPageComponent implements OnInit {
+	
+	rooms: Room[] = [
+		{ value: "restaurant", viewValue: "Restaurant", tables: [] },
+		{ value: "biergarten", viewValue: "Biergarten", tables: ["3", "4", "5"] }
+	]
+
+	
+
+	selected = this.rooms[0];
+
+	ngOnInit(): void {
+		for(var i =0;i<=20;i++){
+			this.selected.tables.push(i.toString());
+		};
+	}
+
+	
+}

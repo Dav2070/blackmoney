d18cb580e3008fd5b3e7b42b8411a15fb7c4d144
David <zurek.dav@gmail.com>
2024-05-01 15:49:50 +0900

    Install & setup apollo-angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,15 @@ import { provideRouter } from "@angular/router"
 import { routes } from "./app.routes"
 import { provideClientHydration } from "@angular/platform-browser"
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async"
+import { provideHttpClient } from "@angular/common/http"
+import { graphqlProvider } from "./graphql.provider"
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideRouter(routes),
 		provideClientHydration(),
-		provideAnimationsAsync()
+		provideAnimationsAsync(),
+		provideHttpClient(),
+		graphqlProvider
 	]
 }

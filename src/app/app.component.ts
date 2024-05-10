9b3905f449186978552b6d480748a8dd26f131a9
David <zurek.dav@gmail.com>
2024-05-10 20:00:42 +0200

    Implement recreating the apollo client with the access token in the auth header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from "@angular/core"
+import { HttpHeaders } from "@angular/common/http"
+import { InMemoryCache } from "@apollo/client/core"
+import { Apollo } from "apollo-angular"
+import { HttpLink } from "apollo-angular/http"
+import { AuthService } from "src/app/services/auth-service"
+import { environment } from "src/environments/environment"
 
 @Component({
 	selector: "app-root",
@@ -6,5 +12,24 @@ import { Component } from "@angular/core"
 	styleUrl: "./app.component.scss"
 })
 export class AppComponent {
-	title = "blackmoney"
+	constructor(
+		private apollo: Apollo,
+		private httpLink: HttpLink,
+		private authService: AuthService
+	) {}
+
+	ngOnInit() {
+		this.apollo.removeClient()
+
+		this.apollo.create({
+			cache: new InMemoryCache(),
+			link: this.httpLink.create({
+				uri: environment.apiUrl,
+				headers: new HttpHeaders().set(
+					"Authorization",
+					this.authService.getAccessToken()
+				)
+			})
+		})
+	}
 }

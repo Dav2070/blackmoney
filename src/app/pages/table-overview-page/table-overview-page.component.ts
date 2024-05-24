40826befa2bbd012aa696942ffd04e9099447aa3
David <zurek.dav@gmail.com>
2024-05-25 00:59:04 +0200

    Implement redirect to login page on TableOverviewPage if user is not logged in

--- src/app/pages/table-overview-page/table-overview-page.component.ts
+++ src/app/pages/table-overview-page/table-overview-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from "@angular/core"
+import { Router } from "@angular/router"
 import { ApiService } from "src/app/services/api-service"
+import { AuthService } from "src/app/services/auth-service"
 import { isServer } from "src/app/utils"
 import { RoomResource } from "src/app/types"
 
@@ -11,10 +13,19 @@ export class TableOverviewPageComponent {
 	rooms: RoomResource[] = []
 	selected: RoomResource = null
 
-	constructor(private apiService: ApiService) {}
+	constructor(
+		private apiService: ApiService,
+		private authService: AuthService,
+		private router: Router
+	) {}
 
 	async ngOnInit() {
 		if (isServer()) return
+
+		if (this.authService.getAccessToken() == null) {
+			this.router.navigate(["login"])
+			return
+		}
 
 		let listRoomsResult = await this.apiService.listRooms(
 			`

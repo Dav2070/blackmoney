33ef899ce106c4dcd512f858a6179980d533c7d2
David <zurek.dav@gmail.com>
2024-06-10 21:00:42 +0200

    Add login with dav button & implement functionality

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,7 +1,13 @@
 import { Component } from "@angular/core"
+import { Dav } from "dav-js"
+import { environment } from "src/environments/environment"
 
 @Component({
 	templateUrl: "./landing-page.component.html",
 	styleUrl: "./landing-page.component.scss"
 })
-export class LandingPageComponent {}
+export class LandingPageComponent {
+	navigateToLoginPage() {
+		Dav.ShowLoginPage(environment.davApiKey, window.location.origin)
+	}
+}

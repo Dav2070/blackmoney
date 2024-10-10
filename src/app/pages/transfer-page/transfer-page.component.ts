b4e3518ec05f0a95d8ab7137a71326be5d3a496d
maaaaiik <M-gruenert99@gmx.de>
2024-10-10 02:02:26 +0200

    Transfer Page Component created

---
+++ src/app/pages/transfer-page/transfer-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transfer-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './transfer-page.component.html',
+  styleUrl: './transfer-page.component.scss'
+})
+export class TransferPageComponent {
+
+}

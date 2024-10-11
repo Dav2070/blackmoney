722a89902dcaf2bd6b8aa155b3df7700a96b9331
maaaaiik <M-gruenert99@gmx.de>
2024-10-11 02:29:46 +0200

    Structure for Transfer-Page

--- src/app/pages/transfer-page/transfer-page.component.ts
+++ src/app/pages/transfer-page/transfer-page.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core"
+import { Item } from "src/app/models/item.model"
+import { Variation } from "src/app/models/variation.model"
 
 @Component({
-  selector: 'app-transfer-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './transfer-page.component.html',
-  styleUrl: './transfer-page.component.scss'
+	templateUrl: "./transfer-page.component.html",
+	styleUrl: "./transfer-page.component.scss"
 })
 export class TransferPageComponent {
+	bookedItems = new Map<Item, Map<Variation, number>>()
+	numberpad: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+	//Berechnet den Preis der hinzugefügten Items
+	calculateTotalPrice(
+		itemPrice: number,
+		variationPrice: number,
+		number: number
+	) {
+		return ((itemPrice + variationPrice) * number).toFixed(2)
+	}
 }

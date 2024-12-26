3ee9afd9ebb50eca609090df2d44444a6c5d0347
David <zurek.dav@gmail.com>
2024-12-26 20:30:22 +0100

    Add staging and production environments

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,8 +1,8 @@
 import { Environment } from "dav-js"
 
 export const environment = {
-	apiUrl: "http://localhost:2000",
-	environment: Environment.Development,
+	apiUrl: "https://blackmoney-api-staging-jwbyi.ondigitalocean.app",
+	environment: Environment.Production,
 	davApiKey: "gHgHKRbIjdguCM4cv5481hdiF5hZGWZ4x12Ur-7v",
 	davAppId: 8
 }

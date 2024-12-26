496421d2aac45eac2a16a139ebbef55ce32118a9
David <zurek.dav@gmail.com>
2024-12-26 18:47:23 +0100

    Add headline to pricing page

--- src/app/pages/landing-pricing-page/landing-pricing-page.component.ts
+++ src/app/pages/landing-pricing-page/landing-pricing-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core"
 
 @Component({
 	templateUrl: "./landing-pricing-page.component.html",
+	styleUrl: "./landing-pricing-page.component.scss",
 	standalone: false
 })
 export class LandingPricingPageComponent {}

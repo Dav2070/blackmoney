e123c287da92f694b9d39a4015cf7f64f9109d5a
David <zurek.dav@gmail.com>
2024-12-27 21:14:59 +0100

    Use staging environment on localhost

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ import { Environment } from "dav-js"
 
 export const environment = {
 	apiUrl: "http://localhost:2000",
-	environment: Environment.Development,
+	environment: Environment.Staging,
 	davApiKey: "gHgHKRbIjdguCM4cv5481hdiF5hZGWZ4x12Ur-7v",
 	davAppId: 8
 }

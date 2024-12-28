8a5223ad5ee7b6c5f98695d9b8bceaee969bafdf
maaaaiik <M-gruenert99@gmx.de>
2024-12-28 20:59:11 +0100

    Refactoring Folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,13 +28,13 @@ import { AppRoutingModule } from "./app-routing.module"
 
 // Components
 import { AppComponent } from "./app.component"
-import { SettingsBarComponent } from "./components/settings-bar/settings-bar.component"
+import { SettingsBarComponent } from "./components/settings/settings-bar/settings-bar.component"
 
 // Pages
 import { LandingPageComponent } from "./pages/landing-pages/landing-page/landing-page.component"
-import { LandingOverviewPageComponent } from "./pages/landing-overview-page/landing-overview-page.component"
+import { LandingOverviewPageComponent } from "./pages/landing-pages/landing-overview-page/landing-overview-page.component"
 import { LandingPricingPageComponent } from "./pages/landing-pages/landing-pricing-page/landing-pricing-page.component"
-import { ForgotPasswordPageComponent } from "./pages/landing-overview-page/forgot-password-page/forgot-password-page.component"
+import { ForgotPasswordPageComponent } from "./pages/landing-pages/landing-overview-page/forgot-password-page/forgot-password-page.component"
 import { LoginPageComponent } from "./pages/landing-pages/login-page/login-page.component"
 import { TableOverviewPageComponent } from "./pages/cash-register-pages/table-overview-page/table-overview-page.component"
 import { BookingPageComponent } from "./pages/cash-register-pages/booking-page/booking-page.component"
@@ -45,9 +45,10 @@ import { AuthService } from "./services/auth-service"
 import { DataService } from "./services/data-service"
 import { TransferPageComponent } from "./pages/cash-register-pages/transfer-page/transfer-page.component"
 import { SeparatePayComponent } from "./pages/cash-register-pages/separate-pay/separate-pay.component"
-import { HeaderComponent } from "./components/header/header.component"
+import { HeaderComponent } from "./components/cash-register/header/header.component"
 import { ServiceWorkerModule } from "@angular/service-worker"
-import { MenuePageComponent } from "./pages/settings-pages/menue-page/menue-page.component"
+import { MenuePageComponent } from "./pages/settings-pages/menue-page/menue-page.component";
+import { SidenavItemsComponent } from './components/settings/sidenav-items/sidenav-items.component'
 
 @NgModule({
 	declarations: [
@@ -66,7 +67,8 @@ import { MenuePageComponent } from "./pages/settings-pages/menue-page/menue-page
 		TransferPageComponent,
 		SeparatePayComponent,
 		//Settings-Pages
-		MenuePageComponent
+		MenuePageComponent,
+  SidenavItemsComponent
 	],
 	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 	bootstrap: [AppComponent],

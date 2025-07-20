d042ecfa987066be278c0fe9a924881c4df3b737
David <zurek.dav@gmail.com>
2025-07-21 00:09:50 +0200

    Add list of restaurants to RestaurantsPage

--- src/app/pages/landing-pages/restaurants-page/restaurants-page.component.ts
+++ src/app/pages/landing-pages/restaurants-page/restaurants-page.component.ts
@@ -1,4 +1,9 @@
 import { Component } from "@angular/core"
+import { Router } from "@angular/router"
+import { Restaurant } from "src/app/models/Restaurant"
+import { ApiService } from "src/app/services/api-service"
+import { DataService } from "src/app/services/data-service"
+import { convertRestaurantResourceToRestaurant } from "src/app/utils"
 
 @Component({
 	templateUrl: "./restaurants-page.component.html",
@@ -6,5 +11,44 @@ import { Component } from "@angular/core"
 	standalone: false
 })
 export class RestaurantsPageComponent {
+	restaurants: Restaurant[] = []
 
+	constructor(
+		private apiService: ApiService,
+		private dataService: DataService,
+		private router: Router
+	) {}
+
+	async ngOnInit() {
+		await this.dataService.blackmoneyUserPromiseHolder.AwaitResult()
+
+		let retrieveCompanyResponse = await this.apiService.retrieveCompany(
+			`
+				restaurants {
+					total
+					items {
+						uuid
+						name
+					}
+				}
+			`
+		)
+
+		if (retrieveCompanyResponse.data == null) return
+
+		const restaurants =
+			retrieveCompanyResponse.data.retrieveCompany.restaurants.items
+
+		for (let restaurant of restaurants) {
+			this.restaurants.push(
+				convertRestaurantResourceToRestaurant(restaurant)
+			)
+		}
+	}
+
+	navigateToRestaurant(event: MouseEvent, restaurant: Restaurant) {
+		event.preventDefault()
+
+		this.router.navigate(["user", "restaurants", restaurant.uuid])
+	}
 }

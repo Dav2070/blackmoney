b1d754feb9b4f751295f11f2bb69d385ec5694b8
David <zurek.dav@gmail.com>
2025-07-21 20:15:35 +0200

    Add basic layout to RestaurantPage & load restaurant

--- src/app/pages/landing-pages/restaurant-page/restaurant-page.component.ts
+++ src/app/pages/landing-pages/restaurant-page/restaurant-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from "@angular/core"
+import { ActivatedRoute } from "@angular/router"
+import { ApiService } from "src/app/services/api-service"
+import { DataService } from "src/app/services/data-service"
 
 @Component({
 	templateUrl: "./restaurant-page.component.html",
@@ -6,5 +9,24 @@ import { Component } from "@angular/core"
 	standalone: false
 })
 export class RestaurantPageComponent {
+	name: string = ""
 
+	constructor(
+		private apiService: ApiService,
+		private dataService: DataService,
+		private activatedRoute: ActivatedRoute
+	) {}
+
+	async ngOnInit() {
+		const uuid = this.activatedRoute.snapshot.paramMap.get("uuid")
+
+		await this.dataService.davUserPromiseHolder.AwaitResult()
+
+		const retrieveRestaurantResponse =
+			await this.apiService.retrieveRestaurant(`name`, { uuid })
+
+		if (retrieveRestaurantResponse.data == null) return
+
+		this.name = retrieveRestaurantResponse.data.retrieveRestaurant.name
+	}
 }

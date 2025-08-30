b9db669c0c65deaebed5d0044ae5fd9478c8553f
David <zurek.dav@gmail.com>
2025-08-30 22:31:39 +0200

    Update production api url

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,7 +1,7 @@
 import { Environment } from "dav-js"
 
 export const environment = {
-	apiUrl: "https://blackmoney-api-staging-jwbyi.ondigitalocean.app",
+	apiUrl: "https://blackmoney-api-tp52n.ondigitalocean.app",
 	environment: Environment.Production,
 	davApiKey: "gHgHKRbIjdguCM4cv5481hdiF5hZGWZ4x12Ur-7v",
 	davAppId: 8

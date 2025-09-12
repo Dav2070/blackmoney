1715d79641d8c40ac5deace7315a7490ad305177
Adam Wegner <adam.wegner213@gmail.com>
2025-09-12 16:20:01 +0200

    Adaptation of OrderItem to include and differentiate between products, menus and specials

--- src/app/models/OrderItem.ts
+++ src/app/models/OrderItem.ts
@@ -1,11 +1,15 @@
 import { Product } from "./Product"
 import { Order } from "./Order"
 import { OrderItemVariation } from "./OrderItemVariation"
+import { Offer } from "./Offer"
 
 export class OrderItem {
 	uuid: string
+	type: "product" | "menu" | "special"
 	count: number
 	order: Order
+	offer?: Offer
 	product: Product
-	orderItemVariations: OrderItemVariation[]
+	orderItems?: OrderItem[]
+	orderItemVariations?: OrderItemVariation[]
 }

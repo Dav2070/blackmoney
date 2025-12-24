5be3c2de9e04b73ed1f489a9d1ebe3ec6fa22279
Adam <adam.wegner213@gmail.com>
2025-12-24 18:33:13 +0100

    merge

---
+++ src/app/models/ReservationDetails.ts
@@ -0,0 +1,10 @@
+export class ReservationDetails {
+	uuid: string
+	numberOfPeople: number
+	reservationDate: Date
+	reservationTime: string
+	name: string
+	phoneNumber?: string
+	email?: string
+	checkedIn: boolean
+}

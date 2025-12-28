8728d9b341171443170d2e2b578bf85c20fe495b
David Zurek <zurek.dav@gmail.com>
2025-12-28 22:46:02 +0100

    Use dav Badge for variation and offer item badges on cards on CategoryPag

--- src/app/pages/landing-pages/category-page/product-card/product-card.component.ts
+++ src/app/pages/landing-pages/category-page/product-card/product-card.component.ts
@@ -1,8 +1,6 @@
 import { Component, EventEmitter, Input, Output } from "@angular/core"
 import { faEllipsis } from "@fortawesome/pro-regular-svg-icons"
 import { Product } from "src/app/models/Product"
-import { Variation } from "src/app/models/Variation"
-import { VariationItem } from "src/app/models/VariationItem"
 import { formatPrice } from "src/app/utils"
 
 @Component({
@@ -16,42 +14,4 @@ export class ProductCardComponent {
 	formatPrice = formatPrice
 	@Input() product: Product = null
 	@Output() optionsButtonClick = new EventEmitter()
-
-	// Variation tooltip overlay state
-	variationTooltipVisible = false
-	variationTooltipItems: VariationItem[] = []
-	variationTooltipTitle = ""
-	variationTooltipX = 0
-	variationTooltipY = 0
-	tooltipHideTimeout: any
-
-	showVariationTooltip(event: MouseEvent, variation: Variation) {
-		if (this.tooltipHideTimeout) {
-			clearTimeout(this.tooltipHideTimeout)
-			this.tooltipHideTimeout = null
-		}
-
-		const target = event.currentTarget as HTMLElement
-		const rect = target.getBoundingClientRect()
-
-		this.variationTooltipTitle = variation.name
-		this.variationTooltipItems = variation.variationItems ?? []
-		this.variationTooltipX = rect.right - 240
-		this.variationTooltipY = rect.bottom + 8
-		this.variationTooltipVisible = true
-	}
-
-	scheduleHideVariationTooltip() {
-		this.tooltipHideTimeout = setTimeout(() => {
-			this.variationTooltipVisible = false
-			this.variationTooltipItems = []
-		}, 120)
-	}
-
-	keepTooltipOpen() {
-		if (this.tooltipHideTimeout) {
-			clearTimeout(this.tooltipHideTimeout)
-			this.tooltipHideTimeout = null
-		}
-	}
 }
